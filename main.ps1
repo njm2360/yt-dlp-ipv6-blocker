5be75b6bc6abeb3c74049ffcdfa47d851d9b754c
njm2360 <me@njm2360.com>
2025-06-17 00:44:37 +0900

    Add multiple programs support

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
-# Google IPv6 Blocker for yt-dlp.exe (VRChat)
+# Google IPv6 Blocker
 
 # AS15169 (Google) IPv6 prefixes list
 $prefixes = @(
@@ -107,31 +107,42 @@ $prefixes = @(
     '2c0f:fb50:4003::/48'
 )
 
-# Firewall rule and target program path
-$ruleName    = 'Block-Google-IPv6-for-yt-dlp'
-$programPath = Join-Path $env:USERPROFILE 'AppData\LocalLow\VRChat\VRChat\Tools\yt-dlp.exe'
+# Firewall apply program list
+$firewallRules = @{
+    "yt-dlp" = Join-Path $env:USERPROFILE 'AppData\LocalLow\VRChat\VRChat\Tools\yt-dlp.exe'
+    "VRChat" = 'C:\Program Files (x86)\Steam\steamapps\common\VRChat\VRChat.exe'
+}
 
-# Check for existing rule
-$rule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction SilentlyContinue
-if (-not $rule) {
-    # Create and enable new rule
-    New-NetFirewallRule -DisplayName $ruleName `
-        -Direction Outbound `
-        -Action Block `
-        -Protocol Any `
-        -RemoteAddress $prefixes `
-        -Profile Any `
-        -Program $programPath
-    Write-Host "Firewall rule '$ruleName' created and Enabled."
-} else {
-    # Toggle state
-    $current = (Get-NetFirewallRule -DisplayName $ruleName).Enabled -eq 'True'
-    if ($current) {
-        Disable-NetFirewallRule -DisplayName $ruleName
-        Write-Host "Firewall rule '$ruleName' is now Disabled."
+# Firewall rule name prefix
+$ruleNamePrefix = 'Block-Google-IPv6-for-'
+
+foreach ($key in $firewallRules.Keys) {
+    $programPath = $firewallRules[$key]
+    $ruleName = "$ruleNamePrefix$key"
+
+    # Check for existing rule
+    $rule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction SilentlyContinue
+
+    if (-not $rule) {
+        # Create and enable new rule
+        New-NetFirewallRule -DisplayName $ruleName `
+            -Direction Outbound `
+            -Action Block `
+            -Protocol Any `
+            -RemoteAddress $prefixes `
+            -Profile Any `
+            -Program $programPath
+        Write-Host "Firewall rule '$ruleName' created and Enabled."
     } else {
-        Enable-NetFirewallRule -DisplayName $ruleName
-        Write-Host "Firewall rule '$ruleName' is now Enabled."
+        # Toggle state
+        $current = ($rule).Enabled -eq 'True'
+        if ($current) {
+            Disable-NetFirewallRule -DisplayName $ruleName
+            Write-Host "Firewall rule '$ruleName' is now Disabled."
+        } else {
+            Enable-NetFirewallRule -DisplayName $ruleName
+            Write-Host "Firewall rule '$ruleName' is now Enabled."
+        }
     }
 }
 
